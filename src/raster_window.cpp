276a3a180a2ec0a319f457b02a84722a2a722f99
Matt Spraggs <matthew.spraggs@gmail.com>
2014-08-09 01:00:29 +0100

    Added swarm graphics rendering to the RasterWindow class

--- src/raster_window.cpp
+++ src/raster_window.cpp
@@ -8,15 +8,47 @@ RasterWindow::RasterWindow(QWindow *parent)
   this->_timer.start(10, this);
 
   this->create();
-  this->setGeometry(100, 100, 480, 480);
+  this->setGeometry(100, 100, 640, 640);
+  
+  this->_boid_poly << QPointF(0, 0);
+  this->_boid_poly << QPointF(-5, -5);
+  this->_boid_poly << QPointF(10, 0);
+  this->_boid_poly << QPointF(-5, 5);
+  this->_boid_poly << QPointF(0, 0);
 }
 
 
 
 void RasterWindow::render(QPainter* painter)
 {
-  painter->drawText(QRectF(0, 0, this->width(), this->height()),
-		    Qt::AlignCenter, QStringLiteral("QWindow"));
+  std::vector<boids::Boid>& swarm = SwarmApplication::instance()->swarm;
+
+  double xscale = double(this->width()) / swarm[0].x_span();
+  double yscale = double(this->height()) / swarm[0].y_span();
+
+  painter->translate(this->width() / 2, this->height() / 2);
+  painter->scale(xscale, -yscale);
+  QBrush brush(Qt::SolidPattern);
+  painter->setPen(QPen(brush, 0.5));
+
+  for (boids::Boid& boid : swarm) {
+    QTransform rotate;
+    rotate.rotateRadians(boid.v_theta());
+    QTransform translate;
+    translate.translate(boid.r_x(), boid.r_y());
+    QPolygonF boid_poly = rotate.map(this->_boid_poly);
+    boid_poly = translate.map(boid_poly);
+    QPainterPath path;
+    path.addPolygon(boid_poly);
+    painter->drawPolygon(boid_poly);
+    painter->fillPath(path, brush);
+    painter->drawEllipse(QPointF(boid.r_x(), boid.r_y()),
+			 boid.sight_range(),
+			 boid.sight_range());
+    painter->drawEllipse(QPointF(boid.r_x(), boid.r_y()),
+			 boid.min_dist(),
+			 boid.min_dist());
+  }
 }
 
 
@@ -92,8 +124,10 @@ void RasterWindow::exposeEvent(QExposeEvent* event)
 
 void RasterWindow::timerEvent(QTimerEvent* event)
 {
-  if (event->timerId() == this->_timer.timerId())
+  if (event->timerId() == this->_timer.timerId()) {
     this->updateSwarm();
+    this->renderNow();
+  }
   else
     QWindow::timerEvent(event);
 }
