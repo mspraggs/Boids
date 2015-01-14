004badf77abf9467f2bf3cdf12f1f2fb85badd13
Matt Spraggs <matthew.spraggs@gmail.com>
2015-01-14 18:55:31 +0000

    Updated utils.hpp and utils.cpp to bring inline with new code

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,22 +3,23 @@
 
 #include <cmath>
 
+#include <Eigen/Dense>
+
+#include <boid.hpp>
+
+
 namespace boids
 {
   namespace math
   {
     const double pi = 3.14159265358979323846;
     
-    const double mod(const double n, const double m);
+    double mod(const double n, const double m);
     
-    const double magnitude(const double x, const double y);
+    double magnitude(const double x, const double y);
 
-    const int sgn(const double x);
+    int sgn(const double x);
   }
-
-  const double correct_coord(const double coord, const double span);
-
-  const double compute_phi(const double x, const double y);
 }
 
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,52 +1,19 @@
 #include <utils.hpp>
 
-namespace boids
-{
-  namespace math
-  {
-    const double mod(const double n, const double m)
-    {
+namespace boids {
+  namespace math {
+    double mod(const double n, const double m) {
       return fmod(fmod(n, m) + m, m);
     }
 
 
-
-    const double magnitude(const double x, const double y)
-    {
+    double magnitude(const double x, const double y) {
       return sqrt(x * x + y * y);
     }
 
 
-
-    const int sgn(const double x)
-    {
+    int sgn(const double x) {
       return (x < 0) ? -1 : 1;
-    }
-  }
-
-
-
-  const double correct_coord(const double coord, const double span)
-  {
-    return math::mod((coord + span / 2), span) - span / 2;
-  }
-
-
-
-  const double compute_phi(const double x, const double y)
-  {
-    double phi = atan(fabs(y / x));
-    if (x > 0) {
-      if (y > 0)
-	return phi;
-      else
-	return -phi;
-    }
-    else {
-      if (y > 0)
-	return math::pi - phi;
-      else
-	return -(math::pi - phi);
     }
   }
 }
